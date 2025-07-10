b99f5cf6ded685f11154d53f48a8e6ae073821fd
wachira-njoroge <solomonwachira212@gmail.com>
2025-07-10 15:01:40 +0300

    Worked on a functionthat cleans user provided contact

---
+++ src/utils/cleanPhone.ts
@@ -0,0 +1,21 @@
+const cleanPhone = (phone: string): string => {
+  let preffix: string = "+254";
+  let result: string = "";
+  phone = phone.replace(/\s/g, "");
+
+  if (phone.charAt(0) == "0") {
+    phone.slice(1);
+    result = preffix + phone.slice(1);
+  } else if (phone.charAt(0) == "7") {
+    result = preffix + phone;
+  } else if (phone.charAt(0) == "2") {
+    result = "+" + phone;
+  } else if (phone.charAt(0) == "+") {
+    result = phone;
+  } else if (phone.charAt(0) == "1") {
+    result = preffix + phone;
+  }
+  return result;
+};
+
+export default cleanPhone;
